8fdb0cc32bd0a1ee5d50cc62e77df54d1d0fbf02
ariyaadinatha <54601298+ariyaadinatha@users.noreply.github.com>
2020-04-27 13:12:03 +0000

    update fungsi bonus 04 dan fungsi jumlah pemain

--- tubes.py
+++ tubes.py
@@ -570,89 +570,158 @@ def bestwahana():
                 print(i+1,"|",arraybaru[i][1],"|",arraybaru[i][2],"|",arraybaru[i][0])
 
 
-def topUp():
-    with open('user.csv') as inf:
-        reader = csv.reader(inf.readlines())
+import csv
 
-    with open('user.csv', 'w') as outf:
-        username=input("Masukkan username : ")
-        jumlahTopUp=int(input("Masukkan saldo : "))
-        writer = csv.writer(outf)
+
+def topUp():
+    with open('user.csv') as topupfile:
+        reader = csv.reader(topupfile.readlines())
+
+    with open('user.csv', 'w') as wtopupfile:
+        username = input("\nMasukkan username : ")
+        jumlahTopUp = int(input("Masukkan saldo : "))
+        writer = csv.writer(wtopupfile)
+        found = 0  # memastikan bahwa data ditemukan
         for line in reader:
-            if username==line[3]:
-                saldoAwal=int(line[6])
-                saldoAkhir=saldoAwal+jumlahTopUp
-                writer.writerow([line[0],line[1],line[2],line[3],line[4],line[5], str(saldoAkhir)])
+            # mencari username
+            if username == line[3]:
+                namaPemain = (line[0])
+                saldoAwal = int(line[6])
+                saldoAkhir = saldoAwal + jumlahTopUp
+                found = found + 1
+                # mengubah data csv
+                writer.writerow([line[0], line[1], line[2], line[3], line[4], line[5], str(saldoAkhir)])
             else:
-                writer.writerow([line[0],line[1],line[2],line[3],line[4],line[5], line[6]])
+                writer.writerow([line[0], line[1], line[2], line[3], line[4], line[5], line[6]])
+    if found == 0:
+        print("\nUser tidak ditemukan")
+    else:
+        print(f"\nTop up berhasil! Saldo {namaPemain} sekarang menjadi {saldoAkhir}")
+
 
 def riwayat():
-    ulang=0
-    arrayJumlahTiket=[]
-    arrayUser=[]
-    arrayTanggal=[]
+    index = 0
+    arrayJumlahTiket = []
+    arrayUser = []
+    arrayTanggal = []
     with open('penggunaan.csv') as tiketfile:
         readTiket = csv.reader(tiketfile)
         next(tiketfile)
-        cariID=input("Masukkan ID wahana : ")
+        cariID = input("\nMasukkan ID wahana : ")
         for baris in readTiket:
-            if cariID==baris[2]:
-                ulang=ulang+1
-                arrayTanggal=arrayTanggal+[baris[1]]
-                arrayJumlahTiket=arrayJumlahTiket+[baris[3]]
-                arrayUser=arrayUser+[baris[0]]
-                
-    print("Riwayat : ")
-    for i in range (ulang):
-        print(f"{arrayTanggal[i]} | {arrayUser[i]} | {arrayJumlahTiket[i]}")
+            if cariID == baris[2]:
+                # menentukan index, agar tidak out of range
+                index = index + 1
+                arrayTanggal = arrayTanggal + [baris[1]]
+                arrayJumlahTiket = arrayJumlahTiket + [baris[3]]
+                arrayUser = arrayUser + [baris[0]]
+    if index == 0:
+        print("Riwayat tidak ditemukan")
+    else:
+        print("Riwayat : ")
+        for i in range(index):
+            print(f"{arrayTanggal[i]} | {arrayUser[i]} | {arrayJumlahTiket[i]}")
 
-def jumlahPemain():
-    #membaca kolom tiket dan jumlah, kemudian menggubahnya ke bentuk array
+
+def jumlahTiket():
+    # membuat array
+    arrayID = []
+    arrayJumlah = []
+    arrayNama = []
+    kamusID = []
+    kamusNama = []
+    banyaKamus = 0
+    catatIndex = []
+    index = 0
     with open('tiket.csv') as tiketfile:
         readTiket = csv.reader(tiketfile)
         next(tiketfile)
-        cari=input("masukkan pencarian : ")
-    	#cari='ariya'
+        cari = input("\nmasukkan username : ")
         for kolom in readTiket:
-            if cari==kolom[0]:
-                arrayID=(kolom[1])
-                arrayJumlah=(kolom[2])
+            # mencari username
+            if cari == kolom[0]:
+                # mengisi array
+                arrayID = arrayID + [kolom[1]]
+                arrayJumlah = arrayJumlah + [kolom[2]]
+                index = index + 1
 
-        arrayKode=(sebutsajaApend(arrayID))
-        print(f"arraykode = {arrayKode}")
+    # untuk mencari nama wahana dari ID
+    with open('wahana.csv') as wahanafile:
+        readWahana = csv.reader(wahanafile)
+        next(wahanafile)
+        for kolom in readWahana:
+            kamusID = kamusID + [kolom[0]]
+            kamusNama = kamusNama + [kolom[1]]
+            banyaKamus = banyaKamus + 1
+        matrixKamus = [kamusID, kamusNama]
+    # menetapkan index wahana
+    for i in range(index):
+        for j in range(banyaKamus):
+            if matrixKamus[0][j] == arrayID[i]:
+                catatIndex = catatIndex + [j]
 
-#mulai dari sini
-    arrayBanyak=[]
-    hitung=0
-    jumlah=0
-#membuat array untuk jumlah tiket
-    for i in arrayJumlah:
-        if i==',':
-            hitung=hitung+1
-        elif i==' ':
-            hitung=hitung+0
-        else:
-            arrayBanyak=arrayBanyak+[f'{i}']
-            jumlah=jumlah+1
+    # menampilkan wahana dan banyak tiket
+    if index == 0:
+        print("User tidak ditemukan")
+    else:
+        print("ID Wahana | Nama Wahana | Banyak Tiket ")
+        for i in range(index):
+            print(f"{arrayID[i]} | {kamusNama[catatIndex[i]]} | {arrayJumlah[i]}")
 
-#menampilkan wahana dan banyak tiket
-    index=hitung+1
-    print("ID Wahana | Banyak Tiket ")
-    for i in range (index):
-        print(f"{arrayKode[i]}    | {arrayBanyak[i]}")
-    
 
 def exitProgram():
     global stmt
-    close=input("\nApakah Anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
-    if close=='Y' or close=='y':
+    close = input("\nApakah Anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
+    if close == 'Y' or close == 'y':
         save()
-        stmt=False
-    elif close=='N' or close=='n':
+        stmt = False
+    elif close == 'N' or close == 'n':
         print("Terima kasih telah menggunakan program ini")
-        stmt=False
+        stmt = False
     else:
         print("Input tidak valid, harap ulangi!")
+
+
+def tiketHilang():
+    # bonus B04
+    with open('tiketHilang.csv') as hilangfile:
+        reader = csv.reader(hilangfile.readlines())
+
+    # menulis kehilangan di file tiketHilang
+    with open('tiketHilang.csv', 'w') as whilangfile:
+        username = input("\nMasukkan username : ")
+        tanggalHilang = input("Tanggal kehilangan tiket : DD/MM/YYYY ")
+        idWahana = input("Masukkan ID wahana : ")
+        jumlahHilang = int(input("Jumlah tiket yang dihilangkan : "))
+        writer = csv.writer(whilangfile)
+
+        for line in reader:
+            writer.writerow([line[0], line[1], line[2], line[3]])
+        writer.writerow([username, tanggalHilang, idWahana, jumlahHilang])
+        print("Laporan kehilangan berhasil disubmit")
+
+    with open('tiket.csv') as tiketfile:
+        reader = csv.reader(tiketfile.readlines())
+
+    # mengurangi tiket di file tiket.csv
+    with open('tiket.csv', 'w') as wtiketfile:
+        writer = csv.writer(wtiketfile)
+        found = 0
+        for line in reader:
+            if username == line[0] and idWahana == line[1]:
+                found = found + 1
+                tiketAwal = int(line[2])
+                tiketAkhir = tiketAwal - jumlahHilang
+                if tiketAkhir <= 0:
+                    print("Tiket habis, data dihapus")
+                else:
+                    writer.writerow([line[0], line[1], str(tiketAkhir)])
+            else:
+                writer.writerow([line[0], line[1], line[2]])
+    if found == 0:
+        print("Data tiket tidak ditemukan")
+    else:
+        print("File tiket berhasil diubah")
 
 
 #============================================================================================================================
